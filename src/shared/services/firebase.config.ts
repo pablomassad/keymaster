2ae7926282b27909f5b55dad7a1cea622005032a
pyp <pyp@github.com>
2018-07-28 19:45:48 -0300

    functions

--- src/shared/services/firebase.config.ts
+++ src/shared/services/firebase.config.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG ={
-   apiKey: "AIzaSyBz1Fop1vo9UEFih4nDmlcqerJ9mV5HCOI",
-   authDomain: "keymaster-c4ee3.firebaseapp.com",
-   databaseURL: "https://keymaster-c4ee3.firebaseio.com",
-   projectId: "keymaster-c4ee3",
-   storageBucket: "keymaster-c4ee3.appspot.com",
-   messagingSenderId: "910397781327"
+   apiKey: "AIzaSyABmFcuEmjDMeEHtEL9cuRP_iwAWpgG-W8",
+   authDomain: "pp-keymaster.firebaseapp.com",
+   databaseURL: "https://pp-keymaster.firebaseio.com",
+   projectId: "pp-keymaster",
+   storageBucket: "pp-keymaster.appspot.com",
+   messagingSenderId: "906588072475"
 };
 
